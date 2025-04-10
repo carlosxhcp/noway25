de8f6d47377b885ed96e8fb4c148393387f701c4
carlosxhcp <carlosxhcp@gmail.com>
2025-04-10 14:13:57 -0300

    a

--- core/settings.py
+++ core/settings.py
@@ -88,10 +88,7 @@ WSGI_APPLICATION = 'core.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': dj_database_url.config(
-        default=os.environ.get('DATABASE_URL'),
-        conn_max_age=600,  # boa prática
-    )
+    'default': dj_database_url.config(default=os.getenv("DATABASE_URL"))
 }
 
 AUTHENTICATION_BACKENDS = [
