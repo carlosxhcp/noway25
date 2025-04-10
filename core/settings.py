7cfdaf613d00142d1d65f458a581b90bcab26906
carlosxhcp <carlosxhcp@gmail.com>
2025-04-10 11:54:42 -0300

    open

--- core/settings.py
+++ core/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'dev-secret-key')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['.vercel.app', '.now.sh', 'localhost']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
