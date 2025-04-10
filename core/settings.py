54571e96987779232035c9503ccd99b9920a6069
carlosxhcp <carlosxhcp@gmail.com>
2025-04-10 12:31:24 -0300

    new navbar

--- core/settings.py
+++ core/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'dev-secret-key')
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
